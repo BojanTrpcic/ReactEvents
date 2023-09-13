13914d9cc32cdc0e307a235ed4d530bb09218dc6
Bojan Trpcic <btrpcic@gmail.com>
2023-09-13 20:59:07 +0200

    homework changes

--- src/App.jsx
+++ src/App.jsx
@@ -9,15 +9,36 @@ const users = [
 ];
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       users: [
+        { name: 'Ivan', years: 30, },
+        { name: 'Marko', years: 35 },
+        { name: 'Ana', years: 25 },
+       ],
+       tekst: "Ćiribu Ćiriba"
+    }
+  }
+
+  uvecajGodine = () => {
+    const newUsers = users.map(user => {
+      return {...user, years: user.years + 30}
+    })
+    this.setState({users: newUsers})
+  }
   render() {
+    const {users, tekst} = this.state
     return (
       <>
         <h1>State</h1>
         <UserClass name={users[0].name} years={users[0].years} />
         <UserFunction name={users[1].name} years={users[1].years} />
         <UserChildren name={users[2].name} years={users[2].years}>
-          a hobi mi je programiranje...
+          {tekst}
         </UserChildren>
+        <button onClick={this.uvecajGodine}>Postaraj ekipu</button>
       </>
     )
   }
